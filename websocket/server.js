9bd0984e2600eac13241175105328e7330c90d8c
fanhh <1825923694@qq.com>
2018-02-08 18:52:57 +0800

    完善请求

--- websocket/server.js
+++ websocket/server.js
@@ -10,27 +10,26 @@ function start(route) {
         var test = url.parse(req.url, true)
         var head = {
             "Content-Type": "text/plain",
-            "Access-Control-Allow-Origin": "http://192.168.10.105:8081",
+            "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Headers": "X-Requested-With,content-type",
-            "Access-Control-Allow-Methods": "PUT,POST,GET,DELETE,OPTIONS",
-            "Access-Control-Allow-Credentials": "true"
+            "Access-Control-Allow-Methods": "PUT,POST,GET,DELETE,OPTIONS"
         }
         res.writeHead(200, head);
-
-        console.log(test.pathname)
+console.log(req.method)
+     //   console.log(test.pathname)
        /* res.write("test:" + route(test.pathname))
         res.write("\n")
         res.write("Hello World")*/
         //res.json({"result":{message:"success"}});
-        console.log(res)
+     //   console.log(res)
         res.end(JSON.stringify({"test": route(test.pathname)}))
     }
 
 
     http.createServer(onRequest).listen(8888)
     console.log('服务器开启')
-    console.log(__filename)
-    console.log(__dirname)
+    //console.log(__filename)
+   // console.log(__dirname)
 }
 
 exports.start = start
